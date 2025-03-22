7c2fd49992922dbf82b43823325969efbf73db25
l1ttps <l1ttps443@gmail.com>
2025-03-22 10:59:11 +0700

    config: database connection
    Adds configuration module to handle environment-specific settings.
    
    Loads environment variables from .env or .env.dev based on the NODE_ENV.
    
    Integrates TypeORM for database interaction, enabling connection
    to a PostgreSQL database.
    
    Adds .env.dev to .gitignore

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,21 @@
+import { EnvType } from './../../common/common.enum';
+import { DatabaseModule } from './../../database/database.module';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${
+        process.env.NODE_ENV === EnvType.DEVELOPMENT ? '.env.dev' : '.env'
+      }`,
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
